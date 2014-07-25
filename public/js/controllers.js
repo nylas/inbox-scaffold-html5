aee18e44e01e431e9300f7cf91b47ead2437a19b
Ben Gotow <bengotow@gmail.com>
2014-07-24 17:19:36 -0700

    Conversion to Angular 1.1.5+ "named controllers" complete

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -11,23 +11,37 @@ var _handleAPIError = function(err) {
   alert(msg);
 };
 
+var _valueForQueryParam = function(param_name) {
+  var search = window.location.search;
+  var tokenStart = search.indexOf(param_name+'=');
+  if (tokenStart != -1) {
+    tokenStart += (param_name+'=').length;
+
+    var tokenEnd = search.indexOf('&', tokenStart);
+    if (tokenEnd == -1) tokenEnd = search.length - tokenStart;
+
+    return search.substr(tokenStart, tokenEnd);
+  } else {
+    return null;
+  }
+}
+
 // Controllers
 
 angular.module('baobab.controllers', ['inbox', 'ngSanitize', 'ngCookies']).
 
 controller('AppCtrl', ['$scope', '$namespaces', '$inbox', '$cookieStore', '$sce', function($scope, $namespaces, $inbox, $cookieStore, $sce) {
+  var self = this;
 
-  $scope.inbox_url = $inbox.baseUrl();
-  $scope.inbox_client_id = $inbox.appId();
-  $scope.inbox_redirect_url = window.location.href;
-  $scope.login_hint = '';
-  $scope.me = null;
+  this.inboxURL = $sce.trustAsResourceUrl($inbox.baseUrl());
+  this.inboxClientID = $inbox.appId();
+  this.inboxRedirectURL = window.location.href;
+  this.loginHint = '';
+  this.me = null;
 
-  $scope.actionUrl = $sce.trustAsResourceUrl($scope.inbox_url)
-
-  $scope.setToken = function(authToken) {
+  this.setToken = function(authToken) {
     if ((authToken == null) || (authToken == ''))
-      return $scope.clearToken();
+      return self.clearToken();
 
     $cookieStore.put('inbox_auth_token', authToken);
     $inbox.withCredentials(true);
@@ -35,71 +49,30 @@ controller('AppCtrl', ['$scope', '$namespaces', '$inbox', '$cookieStore', '$sce'
     $namespaces.updateList();
   }
 
-  $scope.clearToken = function() {
+  this.clearToken = function() {
     $cookieStore.remove('inbox_auth_token');
     $inbox.setRequestHeader('Authorization', ''); 
-    $scope.me = null;
+    self.me = null;
   }
 
-  $scope.valueForQueryParam = function(param_name) {
-    var search = window.location.search;
-    var tokenStart = search.indexOf(param_name+'=');
-    if (tokenStart != -1) {
-      tokenStart += (param_name+'=').length;
-
-      var tokenEnd = search.indexOf('&', tokenStart);
-      if (tokenEnd == -1) tokenEnd = search.length - tokenStart;
-
-      return search.substr(tokenStart, tokenEnd);
-    } else {
-      return null;
-    }
+  this.hasToken = function() {
+    return !!$cookieStore.get('inbox_auth_token');
   }
 
   $namespaces.on('update', function(namespaces) {
     if (namespaces.length > 0)
-      $scope.me = {email_address: namespaces[0].emailAddress};
+      self.me = {email_address: namespaces[0].emailAddress};
     else
-      $scope.me = null
+      self.me = null
   });
 
-  var queryAuthToken = $scope.valueForQueryParam('access_token')
-  $scope.setToken(queryAuthToken || $cookieStore.get('inbox_auth_token'));
+  var queryAuthToken = _valueForQueryParam('access_token')
+  this.setToken(queryAuthToken || $cookieStore.get('inbox_auth_token'));
 
   if (queryAuthToken)
     // We've stored the token in a cookie - wipe it from the location bar
     window.location.href = '/'
 
-}]).
-
-controller('tagsCtrl', ['$scope', '$namespaces', function($scope, $namespaces) {
-  $scope.tags = [];
-
-  $scope.filterByTag = function(tag) {
-    $scope.$root.$broadcast('append-filter', {'tag':tag.tagName});
-  };
-
-  function loadTags(namespace) {
-    namespace.tags().then(function(tags) {
-      $scope.tags = tags;
-    }, _handleAPIError);
-  }
-
-  function update(namespaces) {
-    if (namespaces && namespaces.length) {
-      loadTags(namespaces[0]);
-    } else {
-      $scope.tags = [];
-    }
-  };
-
-  $namespaces.on('update', update);
-  update($namespaces.namespaces);
-
-}]).
-
-controller('threadsCtrl', ['$scope', '$namespaces', function($scope, $namespaces) {
-  
 }]).
 
 
@@ -117,14 +90,33 @@ controller('ComposeCtrl', ['$scope', '$namespaces', function($scope, $namespaces
 
 
 controller('MailCtrl', ['$scope', '$namespaces', function($scope, $namespaces) {
-  $scope.threads = [];
-  $scope.filters = {};
-  $scope.search = '';
-
   var self = this;
   
-  this.selectedThreadMessages = null;
+  this.threads = [];
+  this.tags = [];
+  this.filters = {};
+  this.search = '';
+
   this.selectedThread = null;
+  this.selectedThreadMessages = null;
+
+  // internal methods 
+
+  function update(namespaces) {
+    if (namespaces && namespaces.length) {
+        loadThreads(namespaces[0]);
+        loadTags(namespaces[0]);
+    } else {
+      self.threads = [];
+      self.tags = [];
+    }
+  }
+
+  function loadTags(namespace) {
+    namespace.tags().then(function(tags) {
+      self.tags = tags;
+    }, _handleAPIError);
+  }
 
   function loadThreads(namespace) {
     var _2WeeksAgo = ((new Date().getTime() - 1209600000) / 1000) >>> 0;
@@ -133,22 +125,55 @@ controller('MailCtrl', ['$scope', '$namespaces', function($scope, $namespaces) {
       lastMessageAfter: _2WeeksAgo,
       limit: 1000
     };
-
-    for (var key in $scope.filters) {
-      params[key] = $scope.filters[key];
+    for (var key in self.filters) {
+      params[key] = self.filters[key];
     }
 
     namespace.threads({}, params).then(function(threads) {
       threads.sort(function(a, b) {
         return b.lastMessageDate.getTime() - a.lastMessageDate.getTime();
       });
-      $scope.threads = threads;
+
+      self.selectedThread = null;
+      self.selectedThreadMessages = null;
+      self.threads = threads;
       return threads;
     }, _handleAPIError);
   }
 
+  function appendFiltersToSearch(filtersToAppend) {
+    for (var key in filtersToAppend)
+      self.filters[key] = filtersToAppend[key];
+
+    loadThreads($namespaces.namespaces[0]);
+
+    var filterKeys = Object.keys(self.filters)
+    var search = ''
+    for (var ii = 0; ii < filterKeys.length; ii++)
+      search += filterKeys[ii] + ':' + self.filters[filterKeys[ii]];
+    
+    self.search = search;
+  }
+
+  function updateFiltersWithSearch() {
+    self.filters = {};
+    var search_filters = self.search.split(' '); 
+    for (var ii = 0; ii < search_filters.length; ii++) {
+      var filter_parts = search_filters[ii].split(':');
+      if (filter_parts.length == 2)
+        self.filters[filter_parts[0]] = filter_parts[1];
+    }
+    loadThreads($namespaces.namespaces[0]);
+  }
+  
+  // exposed methods
+
+  this.tagClicked = function(tag) {
+    appendFiltersToSearch({"tag": tag.tagName});
+  };
+
   var selectedNode;
-  this.select = function(thread, event) {
+  this.threadClicked = function(thread, event) {
     if (event) {
       if (event.currentTarget === selectedNode) return;
       angular.element(selectedNode).removeClass('active');
@@ -165,40 +190,11 @@ controller('MailCtrl', ['$scope', '$namespaces', function($scope, $namespaces) {
     }
   }
 
-  function update(namespaces) {
-    if (namespaces && namespaces.length) {
-        loadThreads(namespaces[0]);
-    } else {
-      $scope.threads = [];
-    }
+  this.searchClicked = function() {
+    updateFiltersWithSearch();
   }
 
-  $scope.$root.$on('append-filter', function(event, filtersToAppend){
-    for (var key in filtersToAppend)
-        $scope.filters[key] = filtersToAppend[key];
-
-    loadThreads($namespaces.namespaces[0]);
-
-    var filterKeys = Object.keys($scope.filters)
-    var search = ''
-    for (var ii = 0; ii < filterKeys.length; ii++)
-      search += filterKeys[ii] + ':' + $scope.filters[filterKeys[ii]];
-    
-    $scope.search = search;
-  });
-
-  $scope.updateFiltersWithSearch = function() {
-    $scope.filters = {};
-    var search_filters = $scope.search.split(' '); 
-    for (var ii = 0; ii < search_filters.length; ii++) {
-      var filter_parts = search_filters[ii].split(':');
-      if (filter_parts.length == 2)
-        $scope.filters[filter_parts[0]] = filter_parts[1];
-    }
-    loadThreads($namespaces.namespaces[0]);
-  };
-
-  $scope.tagOrder = function(tag_obj) {
+  this.tagOrder = function(tag_obj) {
     var tag = tag_obj.tagName || tag_obj.name;
     ordered = ["all", "inbox", "archive", "drafts", "spam", "send", "sending", "sent",
                "trash", "starred", "unread", "file", "attachment", "important", "unseen",
@@ -211,7 +207,7 @@ controller('MailCtrl', ['$scope', '$namespaces', function($scope, $namespaces) {
     return ordered.length;
   }
 
-  $scope.tagFilter = function(tag_obj) {
+  this.tagFilter = function(tag_obj) {
     var tag = tag_obj.tagName || tag_obj.name;
     if(tag.match("sending|send|sent|unread|all|unseen|unstarred|replied")) {
         return false;
