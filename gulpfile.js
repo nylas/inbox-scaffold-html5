c752dd8118abdcd777e069498a3211dfe288584d
Ben Gotow <bengotow@gmail.com>
2014-09-30 17:34:20 -0700

    [gulp] New tasks for compiling a static version (for gh-pages)

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ var concat = require('gulp-concat');
 var jscs = require('gulp-jscs');
 var jshint = require('gulp-jshint');
 var coffeelint = require('gulp-coffeelint');
+var coffee = require('gulp-coffee');
+var less = require('gulp-less');
 
 var paths = {
   javascript: [
@@ -20,6 +22,25 @@ var paths = {
   tests: [
     'tests/**/*.js'
   ],
+  less: [
+    'public/css/*.less'
+  ],
+  assets: [
+    'public/*.html',
+    'public/partials/*.html',
+    'public/sound/*',
+    'public/img/*',
+    'public/fonts/*',
+    'public/css/*.css',
+    'public/js/angular-inbox.js',
+    'public/js/FileSaver.js',
+    'public/js/infinite-scroll.js',
+    'public/js/bootstrap-tokenfield.js',
+    'public/js/minievents.js'
+  ],
+  components: [
+    'bower_components/**'
+  ]
 }
 
 gulp.task('default', ['style', 'lint']);
@@ -54,3 +75,35 @@ gulp.task('lint-coffee', function() {
     pipe(coffeelint()).
     pipe(coffeelint.reporter());
 });
+
+// For compiling a minimal version of the app that can be deployed
+// without any server whatsoever to gh-pages or S3:
+
+gulp.task('compile-less', function() {
+  return gulp.src(paths.less)
+    .pipe(less({
+      paths: ['public/css']
+    }))
+    .pipe(gulp.dest('gh-pages/css'));
+});
+
+gulp.task('compile-coffee', function() {
+  return gulp.src(paths.coffeescript)
+    .pipe(coffee())
+    .pipe(gulp.dest('gh-pages/js'));
+});
+
+gulp.task('compile-js', function() {
+  return gulp.src(paths.javascript)
+    .pipe(gulp.dest('gh-pages/js'));
+});
+
+gulp.task('copy-assets', function() {
+  gulp.src(paths.assets, {base: "public"})
+    .pipe(gulp.dest('gh-pages'));
+  gulp.src(paths.components, {base: "bower_components"})
+    .pipe(gulp.dest('gh-pages/components'));
+});
+
+gulp.task('gh-pages', ['compile-less', 'compile-coffee', 'compile-js', 'copy-assets']);
+
--- server.js
+++ server.js
@@ -1,32 +1,17 @@
 var coffeescript = require('coffee-script');
 var coffeeMiddleware = require('coffee-middleware');
 var express = require("express");
+var expressLess = require('express-less');
 var fs = require('fs');
 var logfmt = require("logfmt");
 var request = require('request');
+
 var app = express();
-
-var less = new(require("less").Parser)({
-  paths: [ __dirname + '/public/css']
-});
-
-
-// For development: render less to css with no caching
-app.get("*.less", function(req, res) {
-  var path = __dirname + '/public' + req.url;
-  fs.readFile(path, "utf8", function(err, data) {
-    if (err) return res.send(404, err);
-    less.parse(data, function(err, tree) {
-      if (err) return res.send(400, err);
-      res.type("text/css");
-      res.send(tree.toCSS());
-    });
-  });
-});
 
 app.use(logfmt.requestLogger());
 app.use('/', express.static(__dirname + '/public'));
 app.use('/components', express.static(__dirname + '/bower_components'));
+app.use('/css', expressLess(__dirname + '/public/css'));
 app.use(coffeeMiddleware({
     src: __dirname + '/public'
 }));
