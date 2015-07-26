698cb7833b68c413bd2e615d4f99bfd88cba4122
Evan <thueeliyifan@gmail.com>
2015-07-26 13:47:40 -0700

    organize indent

--- public/js/app.js
+++ public/js/app.js
@@ -108,13 +108,13 @@ define([
     var appIdCookie = 'baobab-app-id=';
     var ca = document.cookie.split(';');
     for(var i=0; i<ca.length; i++) {
-            var c = ca[i];
-            while (c.charAt(0)==' ') c = c.substring(1);
-            if (c.indexOf(appIdCookie) != -1)
-              inboxAppID = c.substring(appIdCookie.length,c.length);
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(appIdCookie) != -1)
+          inboxAppID = c.substring(appIdCookie.length,c.length);
     }
     if (inboxAppID === false)
-    window.location = 'set-app-id.html';
+      window.location = 'set-app-id.html';
     // ---
 
     var url = 'https://api.nylas.com';
